6f15cb602189bf19047f2ae06bff8cf55cc8459f
TapPay Developer <user@example.com>
2025-08-06 22:46:51 -0300

    fix: vites production configuration

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ function getRepoName() {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Set base path for GitHub Pages deployment
-  base: mode === 'production' ? `/${getRepoName()}/` : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
